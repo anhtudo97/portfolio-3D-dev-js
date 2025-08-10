dac32954add6c8dfacfffd9193d8a2de9ca6be0c
Alex Dou <anhtudo97@gmail.com>
2025-08-10 23:02:14 +0700

    feat: update config

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  css: {
+    devSourcemap: true,
+  },
+  server: {
+    port: 3000,
+  },
   plugins: [react()],
 })
