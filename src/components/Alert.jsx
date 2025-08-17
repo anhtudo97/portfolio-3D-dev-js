e4a7912c4a03cd7fc1b8ec945c974f23c0052d99
tdoanh <tdoanh@fivefaces.com.au>
2025-08-18 02:08:36 +0700

    feat: update components

--- src/components/Alert.jsx
+++ src/components/Alert.jsx
@@ -1,7 +1,6 @@
-import { AnimatePresence, motion as Motion } from 'motion/react';
-import { twMerge } from 'tailwind-merge';
+import { motion as Motion, AnimatePresence } from 'motion/react';
 
-const variants = {
+const alertVariants = {
   hidden: { opacity: 0, y: 50, scale: 0.8 },
   visible: { opacity: 1, y: 0, scale: 1 },
   exit: { opacity: 0, y: -50, scale: 0.8 }
@@ -15,20 +14,18 @@ const Alert = ({ type, text }) => {
         initial='hidden'
         animate='visible'
         exit='exit'
-        variants={variants}
+        variants={alertVariants}
         transition={{ duration: 0.3, ease: 'easeInOut' }}
       >
         <div
-          className={twMerge(
-            'p-2 items-center text-indigo-100 leading-none lg:rounded-full flex lg:inline-flex rounded-md',
+          className={`p-2 ${
             type === 'danger' ? 'bg-red-800' : 'bg-royal'
-          )}
+          } items-center text-indigo-100 leading-none lg:rounded-full flex lg:inline-flex rounded-md p-5`}
         >
           <p
-            className={twMerge(
-              'flex rounded-full uppercase px-2 py-1 text-xs font-semibold mr-3',
+            className={`flex rounded-full ${
               type === 'danger' ? 'bg-red-500' : 'bg-lavender'
-            )}
+            } uppercase px-2 py-1 text-xs font-semibold mr-3`}
           >
             {type === 'danger' ? 'Failed' : 'Success'}
           </p>
