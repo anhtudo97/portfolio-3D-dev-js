2130dd664c68aac1ba89b6fc5c9df99e30bff16b
Alex Dou <38291962+anhtudo97@users.noreply.github.com>
2025-08-26 00:34:26 +0700

    feat: update package + UI

--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -13,7 +13,7 @@ const Projects = () => {
   };
   const [preview, setPreview] = useState(null);
   return (
-    <section onMouseMove={handleMouseMove} className='relative c-space section-spacing'>
+    <section id="work" onMouseMove={handleMouseMove} className='relative c-space section-spacing'>
       <h2 className='text-heading'>My Selected Projects</h2>
       <div className='bg-gradient-to-r from-transparent via-neutral-700 to-transparent mt-12 h-[1px] w-full' />
       {myProjects.map((project) => (
